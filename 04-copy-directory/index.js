95f5bc761da7c01083b1ae5dbd0e28ce8fa0c35b
Stanislau Vyrski <vyr4376@yandex.ru>
2023-05-04 21:09:35 +0300

    feat: finish 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
 
+const path = require('path');
+const filePath =  path.join(__dirname, 'files');
+const filePathCopy =  path.join(__dirname, 'files-copy');
+const fs = require('fs');
+const fsPromises = require('fs/promises');
+
+
+
+(async () => {
+  await fsPromises.mkdir(path.join(filePathCopy),
+    { recursive: true });
+  const copyDirection = await fsPromises.readdir(path.join(filePathCopy));
+  for (let file of copyDirection) {
+    await fsPromises.rm(path.join(filePathCopy, file),
+      { force: true });
+  }
+  const copyFiles = await fsPromises.readdir(path.join(filePath));
+  copyFiles.forEach(async(file) => {
+    await fsPromises.copyFile(path.join(filePath, file), path.join(filePathCopy, file));
+  });
+})();
